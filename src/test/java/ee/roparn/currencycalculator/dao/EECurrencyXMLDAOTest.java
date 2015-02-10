87dbdffb234d85f6b7658bcc08f64569034e3b2e
roparn <robert.parn@gmx.com>
2015-02-11 00:28:17 +0200

    Added two unit tests for a method in EECurrencyXMLDAOTest.java for checking if file is received and whether error is thrown in case of any problems.

--- src/test/java/ee/roparn/currencycalculator/dao/EECurrencyXMLDAOTest.java
+++ src/test/java/ee/roparn/currencycalculator/dao/EECurrencyXMLDAOTest.java
@@ -15,19 +15,20 @@ import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 public class EECurrencyXMLDAOTest {
 
-  private CurrencyXMLDAO eeCurrencyXMLDAO;
+  private final String urlString = "http://www.eestipank.ee/valuutakursid/export/xml/latest";
+  private CurrencyXMLDAO eeCurrencyXMLDAO = new EECurrencyXMLDAO();
 
   @Before
   public void setUp() throws Exception {
-    eeCurrencyXMLDAO = new EECurrencyXMLDAO();
   }
 
   @Test
   public void createFileFromURL() throws IOException {
-    eeCurrencyXMLDAO.createFileFromURL("http://www.eestipank.ee/valuutakursid/export/xml/latest");
+    eeCurrencyXMLDAO.createFileFromURL(urlString);
 
     assertFileExistsAndDelete(eeCurrencyXMLDAO.getFileNameWithDateFormat(new Date()));
   }
@@ -35,7 +36,7 @@ public class EECurrencyXMLDAOTest {
   @Test
   public void createFileFromURLWithRequestedDate() throws ParseException, IOException, SAXException {
     Date date = new SimpleDateFormat("dd.MM.yy").parse("12.12.14");
-    eeCurrencyXMLDAO.createFileFromURLWithRequestedDate("http://www.eestipank.ee/valuutakursid/export/xml/latest", date);
+    eeCurrencyXMLDAO.createFileFromURLWithRequestedDate(urlString, date);
 
     assertEquals("12.12.14", new SimpleDateFormat("dd.MM.yy").format(eeCurrencyXMLDAO.getDateFromSavedXML()));
     assertFileExistsAndDelete("currencies_12-12-14.xml");
@@ -46,6 +47,24 @@ public class EECurrencyXMLDAOTest {
     eeCurrencyXMLDAO.XML_FILE = new File(getClass().getResource("/ee-example-currencytable.xml").toURI());
     List<CurrencyModel> currencies = eeCurrencyXMLDAO.getCurrenciesFromSavedXML();
     assertEquals(currencies.size(), 31);
+  }
+
+  @Test
+  public void saveAndParseCurrenciesXML_positiveFlow() throws Exception {
+    List<CurrencyModel> currencyModels = eeCurrencyXMLDAO.saveAndParseCurrenciesXML(urlString);
+
+    assertFileExistsAndDelete(eeCurrencyXMLDAO.getFileNameWithDateFormat(new Date()));
+    assertEquals(31, currencyModels.size());
+  }
+
+  @Test
+  public void saveAndParseCurrenciesXML_badURL() throws Exception {
+    try {
+      List<CurrencyModel> currencyModels = eeCurrencyXMLDAO.saveAndParseCurrenciesXML("URLISBAD");
+      fail("Exception not thrown!");
+    } catch (Exception e) {
+      assertEquals("Error downloading currencies", e.getMessage());
+    }
   }
 
   private void assertFileExistsAndDelete(String expectedFileName) {
